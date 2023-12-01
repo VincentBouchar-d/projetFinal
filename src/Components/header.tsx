c311b8606ac0860dfc8679b94f2fc5433f05e856
Vincent Bouchard <2236622@cegeprdl.ca>
2023-12-01 14:20:20 -0500

    Ajout du react router, l'aventure commence !

---
+++ src/Components/header.tsx
@@ -0,0 +1,11 @@
+import '../styles/header.scss'
+export default function Header() {
+  return (
+    <header>
+        <nav>
+
+        </nav>
+        <h1> je suis le head</h1>
+    </header>
+  )
+}
--- src/Components/produit_component.tsx
+++ src/Components/produit_component.tsx
@@ -1,2 +1,11 @@
 import '../styles/produit.scss'
-
+import Produit from '../Types/produit'
+export default function Produit({produit}: {produit: Produit}) {
+  return (
+    <div >
+            {produit.name}
+            {produit.marque}
+            {produit.price}
+    </div>
+  )
+}
